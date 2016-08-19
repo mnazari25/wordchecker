a83136e8efc07ac2fb0ecdb71a34a9c4fdcfb792
Amir Nazari <amir.nazari@highwinds.com>
2016-08-19 14:46:42 -0400

    Initial Commit

---
+++ Word Checker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Word Checker
+//
+//  Created by Amir Nazari on 8/19/16.
+//  Copyright © 2016 Amir Nazari. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
